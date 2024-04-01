e51e3df376c0693c6a96e4ee386ad08df2f345b9
Claudio <claudio.mori.9510@gmail.com>
2024-03-31 19:49:08 -0500

    Added two endpoints to calculate metrics

--- src/main.py
+++ src/main.py
@@ -52,10 +52,20 @@ def insert_data(data, table_name, headers):
   except Exception as e:
     return {"error": str(e)}  # Return an error message if any exception occurs
 
-# Pydantic model for CSV data
-class CSVData(BaseModel):
-    headers: list[str]
-    data: list[list[str]]
+def compute_metric(query):
+   """Lists the number of employees hired for each job and department in 2021 divided by quarter using SQL."""
+
+   try:
+      with pyodbc.connect(db_connection_string) as conn:
+        with conn.cursor() as cursor:
+            cursor.execute(query)  # Execute the query with each row
+            results = cursor.fetchall()
+      metric_results = [dict(zip([col[0] for col in cursor.description], row)) for row in results]
+
+      return {"message": "Metric computed successfully!", "data": metric_results}
+   
+   except Exception as e:
+      return {"error": str(e)}  # Return an error message if any exception occurs
 
 # API endpoint to retrieve CSV data and insert into SQL
 @app.get("/csv/{filename}")
@@ -86,6 +96,32 @@ async def get_csv_data(filename: str):
         response = insert_data(data, table_name, headers)
         return response
     except Exception as e:
+        # Handle errors appropriately (e.g., return HTTP status code 404)
+        return {"error": str(e)}
+    
+@app.get("/hired_by_quarter")
+async def calculate_first_metric():
+   """Shows the number of employees hired for each job and department in 2021 divided by quarter."""
+
+   query = "SELECT d.department, j.job, SUM(CASE WHEN MONTH(h.datetime) BETWEEN 1 AND 3 THEN 1 ELSE 0 END) AS Q1, SUM(CASE WHEN MONTH(h.datetime) BETWEEN 4 AND 6 THEN 1 ELSE 0 END) AS Q2, SUM(CASE WHEN MONTH(h.datetime) BETWEEN 7 AND 9 THEN 1 ELSE 0 END) AS Q3, SUM(CASE WHEN MONTH(h.datetime) BETWEEN 10 AND 12 THEN 1 ELSE 0 END) AS Q4 FROM hired_employees h INNER JOIN departments d ON d.id = h.department_id INNER JOIN jobs j ON j.id = h.job_id WHERE YEAR(h.datetime) = '2021' GROUP BY d.department, j.job ORDER BY d.department, j.job"
+
+   try:
+      response = compute_metric(query)
+      return response
+   except Exception as e:
+        # Handle errors appropriately (e.g., return HTTP status code 404)
+        return {"error": str(e)}
+   
+@app.get("/most_hired_in_2021")
+async def calculate_second_metric():
+   """Shows which departments hired more employees than the mean of employees hired in 2021 of all departments."""
+
+   query = "with empleados_2021 as (select d.id,  d.department, count(h.id) as empleados from hired_employees h inner join departments d on d.id = h.department_id where year(h.datetime) = '2021' group by d.id, d.department) select  d.id,  d.department, count(h.id) as empleados from hired_employees h inner join departments d on d.id = h.department_id group by d.id, d.department having count(h.id) > (select avg(empleados) as promedio from empleados_2021) order by empleados desc"
+
+   try:
+      response = compute_metric(query)
+      return response
+   except Exception as e:
         # Handle errors appropriately (e.g., return HTTP status code 404)
         return {"error": str(e)}
 
